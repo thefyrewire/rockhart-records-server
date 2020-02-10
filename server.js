aa52e923cb8a915de62b82124f93022bc54b0fb2
thefyrewire <michael-hay369@hotmail.co.uk>
2020-02-10 00:10:55 +0000

    added authorisation endpoints

--- server.js
+++ server.js
@@ -38,15 +38,37 @@ http.listen(5000, () => {
 });
 
 // redirect user to authorize screen
-const auth = (req, res) => {
-  const authorizationUri = `${oauth2.path.authorize}?client_id=${oauth2.client.id}&redirect_uri=${encodeURIComponent(oauth2.path.callback)}&response_type=code`;
+const authLogin = async (req, res) => {
+  const nonce = {
+    token: await uidgen.generate(),
+    redirect: req.query.next || 'http://localhost:3000/dashboard'
+  }
+  const state = jwt.sign(nonce, process.env.JWT_AUTHCODE_SECRET, { expiresIn: '1m', issuer: process.env.JWT_ISSUER });
+  const authorizationUri = `${oauth2.path.authorize}?client_id=${oauth2.client.id}&redirect_uri=${encodeURIComponent(oauth2.path.callback)}&response_type=code&state=${state}`;
 
   res.redirect(authorizationUri);
 }
 
+// log the user out by destroying the cookie
+const authLogout = async (req, res) => {
+  res.cookie('token', '', { expires: new Date(1), path: '/' });
+  res.redirect('http://localhost:3000');
+}
+
 // exchange the auth code for a token
 const callback = async (req, res) => {
-  console.log(req.cookies);
+  const state = req.query.state;
+  let next = 'http://localhost:3000/dashboard';
+
+  try {
+    const decoded = await jwt.verify(state, process.env.JWT_AUTHCODE_SECRET);
+    next = decoded.redirect;
+
+  } catch (error) {
+    console.log(error.message);
+    res.redirect('http://localhost:3000?error=Unauthorised-BadState');
+    return;
+  }
 
   const code = req.query.code;
   console.log(`Code: ${code}`);
@@ -66,30 +88,27 @@ const callback = async (req, res) => {
     console.log(`Token: ${token}`);
     console.log(`JWT: ${jwt_token}`);
 
-    res.cookie('token', jwt_token, { httpOnly: true });
-    res.redirect('http://localhost:3000');
+    res.cookie('token', jwt_token, { secure: false, httpOnly: true }); // set secure to true when on HTTPS
+    res.redirect(next);
 
   } catch (error) {
     console.log(`Access token error: ${error.message}`);
+    res.redirect('http://localhost:3000?error=Unauthorised-BadAuth');
   }
 }
 
-// test endpoint
-const test = (req, res) => {
-  res.json(['foo', 'bar']);
-}
-
 // validate JWT token in cookie
-const validate = (req, res) => {
+const me = (req, res) => {
   const { token } = req.cookies;
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log('Decoded: ', decoded);
-    res.json({ authenticated: true });
-    
+    res.statusCode = 200;
+    res.json({ authenticated: true, user: { name: decoded.user_name, id: decoded.user_id, level: decoded.level } });
+
   } catch (error) {
-    console.log(error.message);
-    res.json({ authenticated: false });
+    console.error(error.message);
+    res.statusCode = 401;
+    res.json({ authenticated: false, user: null });
   }
 }
 
@@ -97,7 +116,7 @@ const validate = (req, res) => {
 app.use('/css', express.static(__dirname + '/public/css/'));
 
 // bind routes to functions
-app.get('/auth', auth);
+app.get('/auth/login', authLogin);
+app.get('/auth/logout', authLogout);
 app.get('/auth/callback', callback);
-app.get('/api/test', test);
-app.get('/auth/validate', validate);
+app.get('/auth/me', me);
