ec79b3bfb3f72285723227ad9ae525deb5c30542
thefyrewire <michael-hay369@hotmail.co.uk>
2020-02-10 23:33:57 +0000

    created Record model

---
+++ models/Record.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecordSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  album_art: {
+    type: String,
+    required: true
+  },
+  spotify_url: {
+    type: String
+  },
+  purchase_url: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    required: true
+  },
+  updated_at: {
+    type: Date,
+    required: true
+  }
+});
+
+RecordSchema.set('toJSON', {
+  transform: (doc, ret, opt) => {
+    const retJSON = {
+      id: ret._id,
+      name: ret.name,
+      artist: ret.artist,
+      album_art: ret.album_art,
+      spotify_url: ret.spotify_url,
+      purchase_url: ret.purchase_url,
+      created_at: ret.created_at,
+      updated_at: ret.updated_at
+    }
+    return retJSON;
+  }
+});
+
+module.exports = User = mongoose.model('record', RecordSchema);
