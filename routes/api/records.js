215cf1a4a7a44b7e940dd2f672d08bc64a1e4d53
thefyrewire <michael-hay369@hotmail.co.uk>
2020-02-14 16:13:00 +0000

    added PUT record functionality

--- routes/api/records.js
+++ routes/api/records.js
@@ -21,7 +21,8 @@ const postRecord = async (req, res) => {
   const { token } = req.cookies;
 
   try {
-    jwt.verify(token, process.env.JWT_SECRET);
+    const { level } = jwt.verify(token, process.env.JWT_SECRET);
+    if (level !== 'admin') throw new Error();
 
   } catch (error) {
     console.log(error.message);
@@ -52,8 +53,42 @@ const postRecord = async (req, res) => {
   }
 }
 
-router.get('/', getRecords);
-router.post('/', [
+const putRecord = async (req, res) => {
+  const { token } = req.cookies;
+
+  try {
+    const { level } = jwt.verify(token, process.env.JWT_SECRET);
+    if (level !== 'admin') throw new Error();
+
+  } catch (error) {
+    console.log(error.message);
+    return res.sendStatus(401);
+  }
+
+  try {
+    const { id } = req.params;
+    const { name, artist, album_art, spotify_url, purchase_url } = req.body;
+
+    const recordUpdated = await Record.findByIdAndUpdate(id , {
+      name,
+      artist,
+      album_art,
+      spotify_url: spotify_url || null,
+      purchase_url: purchase_url || null,
+      updated_at: Date.now()
+    }, { new: true });
+
+    console.log(`Record with ID ${id} updated: ${name} by ${artist}`);
+    console.log(recordUpdated);
+    return res.json({ record: recordUpdated.toJSON() });
+
+  } catch (error) {
+    console.log(error.message);
+    return res.sendStatus(400);
+  }
+}
+
+const recordValidation = [
   body('*').trim(),
   body('name').notEmpty(),
   body('artist').notEmpty(),
@@ -62,6 +97,10 @@ router.post('/', [
   body('purchase_url').isURL({ protocols: ['http', 'https'] }),
   body('created_at').isISO8601(),
   body('updated_at').isISO8601()
-], postRecord);
+];
+
+router.get('/', getRecords);
+router.post('/', recordValidation, postRecord);
+router.put('/:id', recordValidation, putRecord);
 
 module.exports = router;
